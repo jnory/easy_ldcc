1668d057397c94b2abe112200200a8d139512711
Jun-ya Norimatsu <dummy@alinear.jp>
2020-02-14 19:42:51 +0900

    Add license and fix setup.py

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup
 
 setup(
     name='easy_ldcc',
-    version='0.001',
+    version='0.0.1',
     packages=['easy_ldcc'],
     url='https://github.com/jnory/easy_lddc',
-    license='',
+    license='MIT',
     author='Jun-ya NORIMATSU',
     author_email='',
     description='',
@@ -14,4 +14,10 @@ setup(
         "python-dateutil",
         "requests",
     ],
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: Japanese",
+        "Programming Language :: Python :: 3 :: Only",
+    ]
 )
