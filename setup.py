369d35af09c37853dd48ca23fad6aa446200bb71
Jun-ya Norimatsu <dummy@alinear.jp>
2020-02-14 19:38:17 +0900

    Add setup.py and remove requirements.txt

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='easy_ldcc',
+    version='0.001',
+    packages=['easy_ldcc'],
+    url='https://github.com/jnory/easy_lddc',
+    license='',
+    author='Jun-ya NORIMATSU',
+    author_email='',
+    description='',
+    install_requires=[
+        "mecab-python3==0.996.3",
+        "python-dateutil",
+        "requests",
+    ],
+)
