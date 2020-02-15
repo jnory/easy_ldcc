17a74d958265f27c449dbe07d25070a7b936c083
Jun-ya Norimatsu <dummy@alinear.jp>
2020-02-15 18:30:10 +0900

    0.0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='easy_ldcc',
-    version='0.0.1',
+    version='0.0.2',
     packages=['easy_ldcc'],
     url='https://github.com/jnory/easy_lddc',
     license='MIT',
